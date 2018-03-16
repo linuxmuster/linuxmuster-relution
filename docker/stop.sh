754b7cea51f43628d374b2d68a21bf657fe25b1e
Thomas Schmitt <thomas@linuxmuster.net>
2018-03-16 16:47:56 +0000

    - improved postinst script. - changed certificate path.

--- docker/stop.sh
+++ docker/stop.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 #
 # thomas@linuxmuster.net
-# 20180309
+# 20180312
 #
 # stop linuxmuster-relution container
 #
 
 name="linuxmuster-relution"
+dbname="linuxmusterrelution_database_1"
 
 # shutdown container
 echo -n "Shutting down container: "
 /usr/bin/docker stop "$name" || exit 1
+/usr/bin/docker stop "$dbname" || exit 1
 
 # remove container
 echo -n "Removing container: "
